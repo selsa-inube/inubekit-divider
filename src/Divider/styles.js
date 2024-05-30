d9354e9f13221918189b7973bfd94a8e3d4042ec
cmarin001 <cmarin6051@gmail.com>
2024-05-30 11:18:06 -0500

    fix(styles): update in order to use semantic HTML

--- src/Divider/styles.js
+++ src/Divider/styles.js
@@ -1,11 +1,13 @@
 import styled from "styled-components";
 import { inube } from "@inubekit/foundations";
 
-const StyledDivider = styled.div`
+const StyledDivider = styled.hr`
+  border: 0;
   border-top: 2px ${({ $dashed }) => ($dashed ? "dashed" : "solid")}
     ${({ theme }) =>
       theme?.divider?.stroke?.color || inube.divider.stroke.color};
   width: 100%;
+  margin: 0;
 `;
 
 export { StyledDivider };
